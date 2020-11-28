bef1522669ff71760ce8243ce4404697319c20ca
Ed Baker <edwbaker@gmail.com>
2020-11-28 16:39:35 +0000

    New module for bioacoustic index.

---
+++ modules/bi/module.php
@@ -0,0 +1,59 @@
+<?php
+
+function bi_info() {
+  $info = array(
+    "mname" => "bi",
+    "version" => 1.0,
+    "category" => "analysis",
+    "hname" => "Bioacoustic index",
+    "desc" => "Explain how this works...",
+    "ab-plugin" => TRUE,
+    "code" => array(
+      "type" => "package",
+      "language" => "R",
+      "name" => "soundecology",
+      "source" => "CRAN",
+      "function" => "bioacoustic_index()"
+    ),
+    "table" => "analysis-bi",
+    "params" => array(
+      "source" => array(
+        "desc" => "Filter by source",
+        "type" => "string",
+        "default" => "",
+        "column" => "source",
+        "op" => "=",
+      ),
+      "id" => array(
+        "desc" => "filter by id within source",
+        "type" => "string",
+        "default" => "",
+        "column" => "id",
+        "op" => "="
+      ),
+      "startTime" => array(
+        "desc" => "start time(s) to return",
+        "type" => "integer",
+        "default" => 0,
+        "column" => "startTime",
+        "op" => "range"
+      ),
+      "value" => array(
+        "desc" => "Output of analysis",
+        "type" => "integer",
+        "default" => 0,
+        "column" => "value",
+        "op" => "range"
+      ),
+      "output" => array(
+        "desc" => "At present just an array",
+        "type" => "string",
+        "allowed" => array(
+          "JSON"
+        ),
+        "default" => "JSON"
+      )
+    )
+  );
+  return($info);
+}
