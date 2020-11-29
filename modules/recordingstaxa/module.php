30b1bff50c894bdf33e9ac9854f553744fe1c091
Ed Baker <edwbaker@gmail.com>
2020-11-29 17:33:23 +0000

    Recordings-taxa module.

---
+++ modules/recordingstaxa/module.php
@@ -0,0 +1,198 @@
+<?php
+
+function recordingstaxa_info() {
+  $info = array(
+    "mname" => "recordingstaxa",
+    "version" => 1.0,
+    "category" => "data",
+    "table" => "recordings-taxa",
+    "hname" => "Recordings-taxa",
+    "desc" => "Link recordings to taxa",
+    "params" => array(
+      "source" => array(
+        "desc" => "Filter by source",
+        "type" => "string",
+        "default" => "",
+        "column" => "source",
+        "op" => "=",
+        "autocomplete" => TRUE
+      ),
+      "id" => array(
+        "desc" => "filter by id within source",
+        "type" => "string",
+        "default" => "",
+        "column" => "id",
+        "op" => "=",
+        "autocomplete" => TRUE
+      ),
+      "name" => array(
+        "desc" => "filter by recording name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Title",
+        "op" => "contains",
+        "autocomplete" => TRUE
+      ),
+      "taxon" => array(
+        "desc" => "filter by recording taxon",
+        "type" => "string",
+        "default" => "",
+        "column" => "taxon",
+        "op" => "=",
+        "autocomplete" => TRUE
+      ),
+      "filename" => array(
+        "desc" => "filter by file name",
+        "type" => "string",
+        "default" => "",
+        "column" => "file",
+        "op" => "contains"
+      ),
+      "author" => array(
+        "desc" => "author",
+        "type" => "string",
+        "default" => "",
+        "column" => "author",
+        "op" => "contains",
+        "autocomplete" => TRUE
+      ),
+      "post_date" => array(
+        "desc" => "Date the content was uploaded",
+        "type" => "string",
+        "column" => "post_date",
+        "op" => "none"
+      ),
+      "human_size" => array(
+        "desc" => "Human readble size of file",
+        "type" => "string",
+        "column" => "size",
+        "op" => "none"
+      ),
+      "bytes" => array(
+        "desc" => "filter by file size",
+        "type" => "range",
+        "default" => "",
+        "column" => "size_raw",
+        "op" => "range"
+      ),
+      "mime" => array(
+        "desc" => "MIME type of content",
+        "type" => "string",
+        "column" => "type",
+        "default" => "",
+        "op" => "=",
+        "autocomplete" => TRUE
+      ),
+      "recording_type" => array(
+        "desc" => "Used to identify soundscape recordings",
+        "type" => "string",
+        "column" => "NonSpecimen",
+        "default" => "",
+        "op" => "=",
+        "autocomplete" => TRUE
+      ),
+      "date" => array(
+        "desc" => "Date",
+        "type" => "string",
+        "column" => "Date",
+        "default" => "",
+        "op" => "none"
+      ),
+      "time" => array(
+        "desc" => "Time",
+        "type" => "string",
+        "column" => "Time",
+        "default" => "",
+        "op" => "none"
+      ),
+      "duration" => array(
+        "desc" => "filter by duration",
+        "type" => "range",
+        "default" => "",
+        "column" => "Duration",
+        "op" => "range"
+      ),
+      "rank" => array(
+        "desc" => "Taxonomic rank",
+        "type" => "string",
+        "default" => "",
+        "column" => "Rank",
+        "op" => "=",
+        "autocomplete" => TRUE
+      ),
+      "species" => array(
+        "desc" => "Species name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Species",
+        "op" => "="
+      ),
+      "genus" => array(
+        "desc" => "Genus name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Genus",
+        "op" => "="
+      ), 
+      "tribe" => array(
+        "desc" => "Tribe name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Tribe",
+        "op" => "="  
+      ), 
+      "subfamily" => array(
+        "desc" => "Subfamily name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Subfamily",
+        "op" => "="  
+      ), 
+      "family" => array(
+        "desc" => "Family name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Family",
+        "op" => "="  
+      ), 
+      "suborder" => array(
+        "desc" => "Suborder name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Suborder",
+        "op" => "="  
+      ), 
+      "order" => array(
+        "desc" => "Order name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Order",
+        "op" => "="  
+      ), 
+      "class" => array(
+        "desc" => "Class name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Class",
+        "op" => "="  
+      ), 
+      "kingdom" => array(
+        "desc" => "Kingdom name",
+        "type" => "string",
+        "default" => "",
+        "column" => "Kingdom",
+        "op" => "="  
+      ),
+      "output" => array(
+        "desc" => "The format of the returned data",
+        "type" => "string",
+        "allowed" => array(
+          "JSON",
+          "nakedJSON"
+        ),
+        "default" => "JSON"
+      )
+    ),
+  );
+  return($info);
+}
