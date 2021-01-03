0f0cfee51db2b74e31e9b9854f57f2aed6936511
Ed Baker <edwbaker@gmail.com>
2021-01-03 19:58:59 +0000

    Small ingests module.

---
+++ modules/small_ingests/module.php
@@ -0,0 +1,20 @@
+<?php
+
+function small_ingests_info() {
+  $info = array(
+    "mname" => "small_ingests",
+    "version" => 1.0,
+    "category" => "source",
+    "hname" => "Small ingests",
+    "url" => "https://github.com/audioblast/small_ingests",
+    "web_files" => "https://cdn.audioblast.org/files/unp/",
+    "sources" => array(
+      array(
+        "type" => "recordings",
+        "url" => "https://raw.githubusercontent.com/audioblast/small_ingests/main/ColinBirds.csv",
+        "process" => array()
+      )
+    )
+  );
+  return($info);
+}
