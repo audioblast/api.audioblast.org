2143c57e61875ce0996bac6408a46bd1e2797f9b
Ed Baker <edwbaker@gmail.com>
2022-02-16 20:58:03 +0000

    Data module for data counts

---
+++ modules/data/module.php
@@ -0,0 +1,91 @@
+<?php
+
+function data_info() {
+  $info = array(
+    "mname" => "data",
+    "version" => 1.0,
+    "category" => "standalone",
+    "hname" => "Generic access to data information",
+    "desc" => "Provide information about data contained in audioBlast",
+    "endpoints" => array(
+      "fetch_data_counts" => array(
+        "callback" => "data_counts",
+        "desc" => "Returns a count of data items.",
+        "returns" => "data",
+        "params" => array(
+          "output" => array(
+            "desc" => "At present just an array",
+            "type" => "string",
+            "allowed" => array(
+              "JSON"
+            ),
+            "default" => "JSON"
+          ),
+          "source" => array(
+            "desc" => "Filter by source",
+            "type" => "string",
+            "default" => "",
+            "column" => "source",
+            "op" => "=",
+          ),
+          "id" => array(
+            "desc" => "filter by id within source",
+            "type" => "string",
+            "default" => "",
+            "column" => "id",
+            "op" => "="
+          )
+        )
+      ),
+      "list_data" => array(
+        "callback" => "data_list",
+        "desc" => "Returns a list of data types.",
+        "returns" => "data",
+        "params" => array(
+          "output" => array(
+            "desc" => "At present just an array",
+            "type" => "string",
+            "allowed" => array(
+              "JSON"
+            ),
+            "default" => "JSON"
+          )
+        )
+      )
+    )
+  );
+  return($info);
+}
+
+function data_counts($params) {
+  $modules = loadModules();
+  $sql = "SELECT ";
+  $i = 0;
+  $wc = WHEREclause(generateParams($modules["data"]["endpoints"]["fetch_data_counts"], $params));
+  foreach ($modules as $name => $info) {
+    if ($info["category"] != "data") {continue;}
+    if ($i > 0) { $sql .= ", ";}
+    $sql .= "(SELECT COUNT(*) FROM `audioblast`.`".$info["table"]."` ".$wc.") AS `".$info["table"]."`";
+    $i++;
+  }
+  $sql .= " FROM DUAL;";
+
+  global $db;
+  $res = $db->query($sql);
+  $ret = array();
+  while ($row = $res->fetch_assoc()) {
+    $ret["data"]["counts"] = $row;
+  }
+  $ret["data"]["total"] = array_sum($ret["data"]["counts"]);
+  return($ret);
+}
+
+function data_list($params) {
+  $modules = loadModules();
+  $ret = array();
+  foreach ($modules as $name => $info) {
+    if ($info["category"] != "data") {continue;}
+    $ret[] = $name;
+  }
+  return($ret);
+}
