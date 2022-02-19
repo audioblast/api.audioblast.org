32e115929e4b0179d668ad0757e25859374ea45a
Ed Baker <edwbaker@gmail.com>
2022-02-19 20:58:01 +0000

    Refactor.

--- core/core.php
+++ core/core.php
@@ -6,4 +6,8 @@ include("database.php");
 include("speedbird.php");
 =======
 include("input.php")
+<<<<<<< HEAD
 >>>>>>> 1e2c160 (Include input.php.)
+=======
+include("api.php")
+>>>>>>> f8e4707 (Refactor.)
