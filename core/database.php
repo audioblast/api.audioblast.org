b2ac86a052c6eb8d9406a384a8611e4092771e97
Ed Baker <edwbaker@gmail.com>
2022-02-19 20:33:29 +0000

    Refactor.

--- core/database.php
+++ core/database.php
@@ -1,6 +1,6 @@
-
 <?php
 
+//Load database settings.
 if (file_exists("settings/db.php")) {
   include("settings/db.php");
 } else {
@@ -116,60 +116,4 @@ function WHEREclause($filters) {
     $i++;
   }
   return($wc);
-}
-
-function filterMerge($f1, $f2) {
-  if (is_array($f1)) {
-    if (is_array($f2)) {
-      return(array_merge($f1, $f2));
-    } else {
-      return($f1);
-    }
-  }
-}
-
-function filterABrange($column, $value, $type) {
-  $rangesplit = strpos($value, ":");
-  if ($rangesplit != FALSE) {
-    return(array(
-      array(
-        "column" => $column,
-        "op" => ">",
-        "value" => substr($value, 0, $rangesplit),
-        "type" => $type
-      ),
-      array(
-        "column" => $column,
-        "op" => "<",
-        "value" => substr($value, $rangesplit+1),
-        "type" => $type
-      )
-    ));
-  }
-  $firstchar = substr($value, 0, 1);
-  switch($firstchar) {
-    case ">":
-      return(array(array(
-        "column" => $column,
-        "op" => ">",
-        "value" => substr($value, 1),
-        "type" => $type
-      )));
-      break;
-    case "<":
-      return(array(array(
-        "column" => $column,
-        "op" => "<",
-        "value" => substr($value, 1),
-        "type" => $type
-      )));
-      break;
-    default:
-      return(array(array(
-        "column" => $column,
-        "op" => "=",
-        "value" => $value,
-        "type" => $type
-      )));
-  }
 }
