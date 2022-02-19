5f7083e248d994487712b32e018dfa8c35d4aeba
Ed Baker <edwbaker@gmail.com>
2022-02-19 20:34:13 +0000

    Include input.php.

--- core/core.php
+++ core/core.php
@@ -2,4 +2,8 @@
 include("homepage.php");
 include("modules.php");
 include("database.php");
+<<<<<<< HEAD
 include("speedbird.php");
+=======
+include("input.php")
+>>>>>>> 1e2c160 (Include input.php.)
