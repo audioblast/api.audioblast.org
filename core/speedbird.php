8978cd0fe1f0a207e711bb3af92f36b9bc2d8c3c
Ed Baker <edwbaker@gmail.com>
2022-04-08 12:37:47 +0100

    Debug.

--- core/speedbird.php
+++ core/speedbird.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+  Speedbird cache
+
+  This simple cache service stores the results of queries that take significant
+  execution time (i.e. more than a second). The exact queries that make use of
+  the cache is defined where those queries are called, an example are the homepage
+  statistics of http://audioblast.org. The choice of queries and cache durations
+  are chosen in external code.
+
+*/
+
 function speedbird_put($key, $value) {
   $sql = "INSERT INTO speedbird (`key`, `value`) VALUES ('$key', '$value') ON DUPLICATE KEY UPDATE `value`='$value';";
   global $db;
