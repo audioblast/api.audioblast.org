181e461cbe9f0e10449374e94019999cea3a995f
Ed Baker <edwbaker@gmail.com>
2022-04-09 17:35:19 +0100

    Refactor index page.

--- index.php
+++ index.php
@@ -5,27 +5,41 @@ error_reporting(E_ALL);
 
 ini_set('default_charset', 'utf-8');
 
-  include("core/core.php");
+include("core/core.php");
 
-  //What do we need to do?
-  if (isHomepage()) {
-    ?>
-    <html>
-    <head>
-      <title>audioBLAST! API</title>
-      <link rel="stylesheet" href="https://cdn.audioblast.org/ab-api.css">
-    </head>
-    <body>
+//What do we need to do?
+
+$page = "API";
+if (isHomepage()) {$page = "home";}
+if (isEmbedPage()) {$page = "embed";}
+
+if ($page == "API") {
+  moduleAPI($db);
+} else {
+?>
+<html>
+  <head>
+    <title>audioBLAST! API</title>
+    <link rel="stylesheet" href="https://cdn.audioblast.org/ab-api.css">
+  </head>
+  <body>
     <img src="https://cdn.audioblast.org/audioblast_logo.png" />
-    <h1>audioBLAST! API</h1>
-    <?php print(modulesHTML(loadModules())); ?>
-    </body>
-    </html>
     <?php
-  } else if (isEmbedPage()) {
-     embedPage();
-  } else {
-    moduleAPI($db);
-  }
+    switch($page) {
+        case "home":
+          print("<h1>audioBLAST! API</h1>");
+          print(modulesHTML(loadModules()));
+          break;
+        case "embed":
+          print("<h1>Embed API</h1>");
+          embedPage();
+          break;
+    }
+    ?>
+  </body>
+ </html>
 
-  $db->close();
+<?php
+}
+
+$db->close();
