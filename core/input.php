dd4e8036eceb07958f80111af536760964bfac4f
Ed Baker <edwbaker@gmail.com>
2022-04-09 21:13:08 +0100

    Some tidyng up.

--- core/input.php
+++ core/input.php
@@ -33,58 +33,59 @@ function generateParams($params, $inputs) {
   }
   return($ret);
 }
-  function filterMerge($f1, $f2) {
-    if (is_array($f1)) {
-      if (is_array($f2)) {
-        return(array_merge($f1, $f2));
-      } else {
-        return($f1);
-      }
-    }
-  }
 
-  function filterABrange($column, $value, $type) {
-    $rangesplit = strpos($value, ":");
-    if ($rangesplit != FALSE) {
-      return(array(
-        array(
-          "column" => $column,
-          "op" => ">",
-          "value" => substr($value, 0, $rangesplit),
-          "type" => $type
-        ),
-        array(
-          "column" => $column,
-          "op" => "<",
-          "value" => substr($value, $rangesplit+1),
-          "type" => $type
-        )
-      ));
-    }
-    $firstchar = substr($value, 0, 1);
-    switch($firstchar) {
-      case ">":
-        return(array(array(
-          "column" => $column,
-          "op" => ">",
-          "value" => substr($value, 1),
-          "type" => $type
-        )));
-        break;
-      case "<":
-        return(array(array(
-          "column" => $column,
-          "op" => "<",
-          "value" => substr($value, 1),
-          "type" => $type
-        )));
-        break;
-      default:
-        return(array(array(
-          "column" => $column,
-          "op" => "=",
-          "value" => $value,
-          "type" => $type
-        )));
+function filterMerge($f1, $f2) {
+  if (is_array($f1)) {
+    if (is_array($f2)) {
+      return(array_merge($f1, $f2));
+    } else {
+      return($f1);
     }
   }
+}
+
+function filterABrange($column, $value, $type) {
+  $rangesplit = strpos($value, ":");
+  if ($rangesplit != FALSE) {
+    return(array(
+      array(
+        "column" => $column,
+        "op" => ">",
+        "value" => substr($value, 0, $rangesplit),
+        "type" => $type
+      ),
+      array(
+        "column" => $column,
+        "op" => "<",
+        "value" => substr($value, $rangesplit+1),
+        "type" => $type
+      )
+    ));
+  }
+  $firstchar = substr($value, 0, 1);
+  switch($firstchar) {
+    case ">":
+      return(array(array(
+        "column" => $column,
+        "op" => ">",
+        "value" => substr($value, 1),
+        "type" => $type
+      )));
+      break;
+    case "<":
+      return(array(array(
+        "column" => $column,
+        "op" => "<",
+        "value" => substr($value, 1),
+        "type" => $type
+      )));
+      break;
+    default:
+      return(array(array(
+        "column" => $column,
+        "op" => "=",
+        "value" => $value,
+        "type" => $type
+      )));
+  }
+}
