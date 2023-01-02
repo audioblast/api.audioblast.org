f1a59b4c694193052995bdbee3899dfb2e593bb9
Ed Baker <edwbaker@gmail.com>
2023-01-02 14:48:12 +0000

    Pythia initial commit

---
+++ modules/pythia/module.php
@@ -0,0 +1,42 @@
+<?php
+
+function pythia_info() {
+  $info = array(
+    "mname" => "pythia",
+    "version" => 1.0,
+    "category" => "standalone",
+    "hname" => "Pythia search term processor",
+    "desc" => "Oracle for providing search hints.",
+    "endpoints" => array(
+      "process" => array(
+        "callback" => "pythia_process",
+        "desc" => "Provides search hints.",
+        "returns" => "data",
+        "params" => array(
+          "query" => array(
+            "desc" => "Query string",
+            "type" => "string",
+            "op" => "=",
+            "default" => "",
+          ),
+          "output" => array(
+            "desc" => "At present just an array",
+            "type" => "string",
+            "allowed" => array(
+              "JSON"
+            ),
+            "default" => "JSON"
+          )
+        )
+      )
+    )
+  );
+  return($info);
+}
+
+function pythia_process($f) {
+  $parts = explode($f["query"]);
+  print_r($parts);
+  exit;
+  return($ret);
+}
