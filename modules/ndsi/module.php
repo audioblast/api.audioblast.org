a4566400dfdc318f9cb3690f3fc65cd3e632bd02
Ed Baker <edwbaker@gmail.com>
2023-02-05 20:05:35 +0000

    NDSI

---
+++ modules/ndsi/module.php
@@ -0,0 +1,76 @@
+<?php
+
+function ndsi_info() {
+  $info = array(
+    "mname" => "ndsi",
+    "version" => 1.0,
+    "category" => "analysis",
+    "hname" => "NDSI",
+    "desc" => "Explain how this works...",
+    "ab-plugin" => TRUE,
+    "code" => array(
+      "type" => "package",
+      "language" => "R",
+      "name" => "seewave",
+      "source" => "CRAN",
+      "function" => "NDSI()"
+    ),
+    "table" => "analysis-ndsi",
+    "params" => array(
+      "source" => array(
+        "desc" => "Filter by source",
+        "type" => "string",
+        "default" => "",
+        "column" => "source",
+        "op" => "=",
+      ),
+      "id" => array(
+        "desc" => "filter by id within source",
+        "type" => "string",
+        "default" => "",
+        "column" => "id",
+        "op" => "="
+      ),
+      "startTime" => array(
+        "desc" => "start time(s) to return",
+        "type" => "integer",
+        "default" => 0,
+        "column" => "startTime",
+        "op" => "range"
+      ),
+      "value" => array(
+        "desc" => "Output of analysis",
+        "type" => "integer",
+        "default" => 0,
+        "column" => "value",
+        "op" => "range"
+      ),
+      "channel" => array(
+        "desc" => "Audio channel analysed",
+        "type" => "integer",
+        "default" => 1,
+        "column" => "channel",
+        "op" => "="
+      ),
+      "duration" => array(
+        "desc" => "Analysis duration",
+        "type" => "integer",
+        "default" => 60,
+        "column" => "duration",
+        "op" => "=",
+        "allowed" => array(
+          60
+        )
+      ),
+      "output" => array(
+        "desc" => "At present just an array",
+        "type" => "string",
+        "allowed" => array(
+          "JSON"
+        ),
+        "default" => "JSON"
+      )
+    )
+  );
+  return($info);
+}
