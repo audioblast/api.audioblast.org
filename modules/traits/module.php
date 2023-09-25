577b3d26509b63e687f494b14724073a20139070
Ed Baker <edwbaker@gmail.com>
2023-09-25 12:39:50 +0000

    Trait contains

--- modules/traits/module.php
+++ modules/traits/module.php
@@ -39,7 +39,7 @@ function traits_info() {
         "type" => "string",
         "default" => "",
         "column" => "Trait",
-        "op" => "=",
+        "op" => "contains",
         "autocomplete" => TRUE
       ),
       "trait_ontology" => array(
