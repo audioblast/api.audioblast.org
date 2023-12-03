f436aea9d2d7a9f9c7b271ca3440976805403bd7
Ed Baker <edwbaker@gmail.com>
2023-12-03 18:30:51 +0000

    birdnet default embeddings

---
+++ modules/birdnet_embeddings/module.php
@@ -0,0 +1,52 @@
+<?php
+
+function birdnet_embeddings_info() {
+  $info = array(
+    "mname" => "birdnet_embeddings",
+    "version" => 1.0,
+    "category" => "analysis",
+    "hname" => "Birdnet identifications (default)",
+    "desc" => "",
+    "table" => "analysis_3sec-birdnet_default_embed",
+    
+    "params" => array(
+      "source" => array(
+        "desc" => "Filter by source",
+        "type" => "string",
+        "default" => "",
+        "column" => "source",
+        "op" => "=",
+      ),
+      "id" => array(
+        "desc" => "filter by id within source",
+        "type" => "string",
+        "default" => "",
+        "column" => "id",
+        "op" => "="
+      ),
+      "startTime" => array(
+        "desc" => "start time(s) to return",
+        "type" => "integer",
+        "default" => 0,
+        "column" => "startTime",
+        "op" => "range"
+      ),
+      "value" => array(
+        "desc" => "Output of analysis",
+        "type" => "integer",
+        "default" => 0,
+        "column" => "tdsc",
+        "op" => "range"
+      ),
+      "output" => array(
+        "desc" => "At present just an array",
+        "type" => "string",
+        "allowed" => array(
+          "JSON"
+        ),
+        "default" => "JSON"
+      )
+    ),
+  );
+  return($info);
+}
