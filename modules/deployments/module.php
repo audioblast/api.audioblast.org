663cedaa1778137e03bc244a9eab8ccd4d7b62aa
Ed Baker <edwbaker@gmail.com>
2023-12-08 12:08:29 +0000

    Update deployments fields

--- modules/deployments/module.php
+++ modules/deployments/module.php
@@ -9,14 +9,6 @@ function deployments_info() {
     "hname" => "Deployments",
     "desc" => "This endpoint allows for listing equipment deployments.",
     "params" => array(
-      "source" => array(
-        "desc" => "Source",
-        "type" => "string",
-        "default" => "",
-        "column" => "source",
-        "op" => "=",
-        "autocomplete" => TRUE
-      ),
       "id" => array(
         "desc" => "ID",
         "type" => "string",
@@ -31,54 +23,6 @@ function deployments_info() {
         "default" => "",
         "column" => "name",
         "op" => "contains",
-        "autocomplete" => TRUE
-      ),
-      "device" => array(
-        "desc" => "Device ID",
-        "type" => "string",
-        "default" => "",
-        "column" => "device",
-        "op" => "=",
-        "autocomplete" => TRUE
-      ),
-      "type" => array(
-        "desc" => "Type of deployment",
-        "type" => "string",
-        "default" => "",
-        "column" => "type",
-        "op" => "=",
-        "autocomplete" => FALSE,
-        "allowed" => array(
-          "static",
-          "dynamic"
-        )
-      ),
-      "start" => array(
-        "desc" => "Start of deployment",
-        "type" => "range",
-        "default" => "",
-        "column" => "start",
-        "op" => "range"
-      ),
-      "end" => array(
-        "desc" => "End of deployment",
-        "type" => "range",
-        "default" => "",
-        "column" => "end",
-        "op" => "range",
-        "autocomplete" => FALSE
-      ),
-      "continues_from" => array(
-        "desc" => "Continues form previous deployment",
-        "type" => "string",
-        "column" => "continues_from",
-        "op" => "="
-      ),
-      "group" => array(
-        "desc" => "Deployment group",
-        "type" => "string",
-        "column" => "group",
-        "op" => "=",
         "autocomplete" => TRUE
       ),
       "output" => array(
