25e7528baf54b290ebabef714fe98d397d548b3d
Ed Baker <edwbaker@gmail.com>
2026-06-09 11:35:42 +0100

    Escape embed params to fix reflected XSS
    embedPage() built the embed URL from raw $_GET[source]/$_GET[id] and
    echoed it into an iframe src inside a <textarea>, allowing a
    </textarea> breakout (e.g. source=</textarea><script>...). URL-encode
    the params for the query-string/HTTP context and htmlspecialchars()
    (ENT_QUOTES) the URL for the HTML-attribute context.

--- core/embed.php
+++ core/embed.php
@@ -16,19 +16,22 @@ function isEmbedPage() {
 
 function embedPage() {
   $embeds = json_decode(file_get_contents("http://api.audioblast.org/standalone/modules/list_embeds/?output=nakedJSON"));
+  // URL-encode user-controlled query params before placing them into the embed URL.
+  $source = urlencode($_GET["source"] ?? "");
+  $id = urlencode($_GET["id"] ?? "");
   foreach ($embeds as $modname => $modinfo) {
     print("<h2>".$modname."</h2>");
     foreach ($modinfo as $epname  => $epinfo) {
       print("<h3>".$epname."</h3>");
       foreach ($epinfo->params->output->allowed as $output) {
         print("<h4>".$output."</h4>");
-        $url = "http://api.audioblast.org/embed/".$modname."/".$epname."/?source=".$_GET["source"]."&id=".$_GET["id"]."&output=".$output;
+        $url = "http://api.audioblast.org/embed/".$modname."/".$epname."/?source=".$source."&id=".$id."&output=".$output;
         $response = file_get_contents($url);
         print($response);
         print("<br><textarea style='width: 100%'>");
         switch ($epinfo->embed) {
           case "iframe":
-            print("<iframe src='".$url."' style='border: none; width: 100%;'></iframe>");
+            print("<iframe src='".htmlspecialchars($url, ENT_QUOTES)."' style='border: none; width: 100%;'></iframe>");
         }
         print("</textarea>");
       }
